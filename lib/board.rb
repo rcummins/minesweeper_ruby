dd59213c02a6a92bd54025b0b2912febc4cbddc1
Renata Cummins <renatacummins@gmail.com>
2019-03-28 16:50:41 -0400

    Add Board#initialize and have it make a grid of tiles

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,11 @@
 # implement the Board class for the minesweeper game
 
 require_relative 'tile'
+
+class Board
+    attr_reader :grid
+
+    def initialize
+        @grid = Array.new(9) { Array.new(9) { Tile.new } }
+    end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -2,3 +2,20 @@
 
 require 'RSpec'
 require 'board'
+
+describe 'Board' do
+    let(:board) { Board.new }
+
+    describe '#initialize' do
+        it 'sets @grid to a 9x9 2D array' do
+            expect(board.grid.class).to eq(Array)
+            expect(board.grid[0].class).to eq(Array)
+            expect(board.grid.length).to eq(9)
+            expect(board.grid[0].length).to eq(9)
+        end
+
+        it 'sets @grid to a 2D array containing tiles' do
+            expect(board.grid[0][0].class).to eq(Tile)
+        end
+    end
+end
