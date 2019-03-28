2cc0a3dee5c414c5a70dc284b4ccb1982be068c2
Renata Cummins <renatacummins@gmail.com>
2019-03-28 16:21:17 -0400

    Add Tile#has_bomb?, Tile#revealed?, Tile#flagged?

--- lib/tile.rb
+++ lib/tile.rb
@@ -1,11 +1,21 @@
 # implement the Tile class for the minesweeper game
 
 class Tile
-    attr_reader :has_bomb, :revealed, :flagged
-
     def initialize
         @has_bomb = false
         @revealed = false
         @flagged = false
+    end
+
+    def has_bomb?
+        @has_bomb
+    end
+
+    def revealed?
+        @revealed
+    end
+
+    def flagged?
+        @flagged
     end
 end
--- spec/tile_spec.rb
+++ spec/tile_spec.rb
@@ -7,16 +7,23 @@ describe 'Tile' do
     let(:tile) { Tile.new }
 
     describe '#initialize' do
-        it 'sets the instance variable @has_bomb to false' do
-            expect(tile.has_bomb).to eq(false)
+        it 'sets the instance variable @has_bomb? to false' do
+            expect(tile.has_bomb?).to eq(false)
         end
 
-        it 'sets the instance variable @revealed to false' do
-            expect(tile.revealed).to eq(false)
+        it 'sets the instance variable @revealed? to false' do
+            expect(tile.revealed?).to eq(false)
         end
 
-        it 'sets the instance variable @flagged to false' do
-            expect(tile.flagged).to eq(false)
+        it 'sets the instance variable @flagged? to false' do
+            expect(tile.flagged?).to eq(false)
+        end
+    end
+
+    describe '#add_bomb' do
+        xit 'changes the instance variable @has_bomb? to true' do
+            tile.add_bomb
+            expect(tile.has_bomb?).to eq(true)
         end
     end
 end
