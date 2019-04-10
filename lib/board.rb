fdee22667fa02f3d2a46d6b887820af596be0925
Renata Cummins <renatacummins@gmail.com>
2019-04-10 11:45:04 -0400

    Add Board#add_bombs_to_grid to add 10 bombs to grid and Board#bomb_count

--- lib/board.rb
+++ lib/board.rb
@@ -8,4 +8,16 @@ class Board
     def initialize
         @grid = Array.new(9) { Array.new(9) { Tile.new } }
     end
+
+    def add_bombs_to_grid
+        until bomb_count == 10
+            row = rand(9)
+            column = rand(9)
+            @grid[row][column].add_bomb
+        end
+    end
+
+    def bomb_count
+        @grid.flatten.count { |tile| tile.has_bomb? }
+    end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -18,4 +18,29 @@ describe 'Board' do
             expect(board.grid[0][0].class).to eq(Tile)
         end
     end
+
+    describe '#add_bombs_to_grid' do
+        it 'adds bombs to 10 tiles in the grid' do
+            board.add_bombs_to_grid
+            bomb_count = board.grid.flatten.count { |tile| tile.has_bomb? }
+            expect(bomb_count).to eq(10)
+        end
+    end
+
+    describe '#bomb_count' do
+        it 'returns zero before bombs are added to grid' do
+            expect(board.bomb_count).to eq(0)
+        end
+
+        it 'returns 1 if one bomb has been added to grid' do
+            board.grid[0][0].add_bomb
+            expect(board.bomb_count).to eq(1)
+        end
+
+        it 'returns 2 if two bombs have been added to grid' do
+            board.grid[0][0].add_bomb
+            board.grid[0][1].add_bomb
+            expect(board.bomb_count).to eq(2)
+        end
+    end
 end
