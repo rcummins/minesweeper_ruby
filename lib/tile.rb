980fc9175fd0637f795139614172d1ede85c0221
Renata Cummins <renatacummins@gmail.com>
2019-04-10 13:28:24 -0400

    Add Tile#neighbors and redefine Tile#inspect to be shorter

--- lib/tile.rb
+++ lib/tile.rb
@@ -41,4 +41,35 @@ class Tile
             @flagged = false
         end
     end
+
+    def neighbors
+        all_neighbors = []
+
+        (-1..1).each do |row_offset|
+            (-1..1).each do |column_offset|
+                next if row_offset == 0 && column_offset == 0
+
+                new_row = @row + row_offset
+                new_column = @column + column_offset
+
+                next if new_row < 0 || new_column < 0
+                next if new_row > 8 || new_column > 8
+
+                all_neighbors << @board.grid[new_row][new_column]
+            end
+        end
+
+        return all_neighbors
+    end
+
+    def inspect
+        output = ''
+        output += "row: #{@row}, "
+        output += "column: #{@column}, "
+        output += "has bomb: #{@has_bomb}, "
+        output += "revealed: #{@revealed}, "
+        output += "flagged: #{@flagged}"
+        output += "\n"
+        return output
+    end
 end
--- spec/tile_spec.rb
+++ spec/tile_spec.rb
@@ -5,7 +5,7 @@ require 'tile'
 
 describe 'Tile' do
     let(:board) { Board.new }
-    let(:tile) { Tile.new(board, 1, 1) }
+    let(:tile) { board.grid[1][1] }
 
     describe '#initialize' do
         it 'sets the instance variable @has_bomb to false' do
@@ -57,6 +57,88 @@ describe 'Tile' do
             tile.flag
             tile.flag
             expect(tile.flagged?).to eq(false)
+        end
+    end
+
+    describe '#neighbors' do
+        it 'returns the correct neighbors for an upper left corner tile' do
+            neighbors = board.grid[0][0].neighbors
+            expect(neighbors.length).to eq(3)
+            expect(neighbors[0].row).to eq(0)
+            expect(neighbors[0].column).to eq(1)
+            expect(neighbors[1].row).to eq(1)
+            expect(neighbors[1].column).to eq(0)
+            expect(neighbors[2].row).to eq(1)
+            expect(neighbors[2].column).to eq(1)
+        end
+
+        it 'returns the correct neighbors for an upper right corner tile' do
+            neighbors = board.grid[0][8].neighbors
+            expect(neighbors.length).to eq(3)
+            expect(neighbors[0].row).to eq(0)
+            expect(neighbors[0].column).to eq(7)
+            expect(neighbors[1].row).to eq(1)
+            expect(neighbors[1].column).to eq(7)
+            expect(neighbors[2].row).to eq(1)
+            expect(neighbors[2].column).to eq(8)
+        end
+
+        it 'returns the correct neighbors for a lower left corner tile' do
+            neighbors = board.grid[8][0].neighbors
+            expect(neighbors.length).to eq(3)
+            expect(neighbors[0].row).to eq(7)
+            expect(neighbors[0].column).to eq(0)
+            expect(neighbors[1].row).to eq(7)
+            expect(neighbors[1].column).to eq(1)
+            expect(neighbors[2].row).to eq(8)
+            expect(neighbors[2].column).to eq(1)
+        end
+
+        it 'returns the correct neighbors for a lower right corner tile' do
+            neighbors = board.grid[8][8].neighbors
+            expect(neighbors.length).to eq(3)
+            expect(neighbors[0].row).to eq(7)
+            expect(neighbors[0].column).to eq(7)
+            expect(neighbors[1].row).to eq(7)
+            expect(neighbors[1].column).to eq(8)
+            expect(neighbors[2].row).to eq(8)
+            expect(neighbors[2].column).to eq(7)
+        end
+
+        it 'returns the correct neighbors for an edge tile' do
+            neighbors = board.grid[0][4].neighbors
+            expect(neighbors.length).to eq(5)
+            expect(neighbors[0].row).to eq(0)
+            expect(neighbors[0].column).to eq(3)
+            expect(neighbors[1].row).to eq(0)
+            expect(neighbors[1].column).to eq(5)
+            expect(neighbors[2].row).to eq(1)
+            expect(neighbors[2].column).to eq(3)
+            expect(neighbors[3].row).to eq(1)
+            expect(neighbors[3].column).to eq(4)
+            expect(neighbors[4].row).to eq(1)
+            expect(neighbors[4].column).to eq(5)
+        end
+
+        it 'returns the correct neighbors for a central tile' do
+            neighbors = board.grid[4][4].neighbors
+            expect(neighbors.length).to eq(8)
+            expect(neighbors[0].row).to eq(3)
+            expect(neighbors[0].column).to eq(3)
+            expect(neighbors[1].row).to eq(3)
+            expect(neighbors[1].column).to eq(4)
+            expect(neighbors[2].row).to eq(3)
+            expect(neighbors[2].column).to eq(5)
+            expect(neighbors[3].row).to eq(4)
+            expect(neighbors[3].column).to eq(3)
+            expect(neighbors[4].row).to eq(4)
+            expect(neighbors[4].column).to eq(5)
+            expect(neighbors[5].row).to eq(5)
+            expect(neighbors[5].column).to eq(3)
+            expect(neighbors[6].row).to eq(5)
+            expect(neighbors[6].column).to eq(4)
+            expect(neighbors[7].row).to eq(5)
+            expect(neighbors[7].column).to eq(5)
         end
     end
 end
