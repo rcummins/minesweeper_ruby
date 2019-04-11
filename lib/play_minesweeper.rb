5f67f717c842b9e4c3a23bd6b3147931ed987308
Renata Cummins <renatacummins@gmail.com>
2019-04-11 14:45:37 -0400

    Modify logic to evaluate win/lose before revealing whole board to fix automatic lose bug

--- lib/play_minesweeper.rb
+++ lib/play_minesweeper.rb
@@ -8,10 +8,10 @@ until game.game_over?
     game.take_turn
 end
 
-game.display_when_game_over
-
 if game.won?
     puts "You won!"
 else
     puts "Sorry, you lost"
 end
+
+game.display_when_game_over
